db043fab86727e44240e3090f09c79c5070db35b
LiiQLiiQato77 <buruuj95@gmail.com>
2026-08-02 19:29:41 +0300

    feat: update DOM content with button click

---
+++ Exercise 33/exercise33.js
@@ -0,0 +1,18 @@
+// Exercise 33
+
+const name = document.querySelector('h1');
+
+const description = document.querySelector('#paragraph');
+
+function changeContent () {
+
+    name.textContent = `Real_Dhagax`
+
+};
+
+function changeElement () {
+
+    description.innerHTML = `<strong> Lorem </strong> ipsum <i> dolor </i> sit amet`
+    
+}
+
