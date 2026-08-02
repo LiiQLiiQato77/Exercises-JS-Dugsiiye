06114cc4e02d08774659ddf4f186c06a517258af
LiiQLiiQato77 <buruuj95@gmail.com>
2026-08-02 19:25:56 +0300

    feat: practice selecting DOM elements with query selectors

---
+++ Exercise 32/exercise32.js
@@ -0,0 +1,21 @@
+// Exercise 32
+
+const name = document.querySelector('h1');
+
+console.log(name);
+
+const real = document.querySelector('.nickname');
+
+console.log(real);
+
+const paraghraph = document.querySelector('#desc');
+
+console.log(paraghraph);
+
+const picture = document.querySelector('img');
+
+console.log(picture);
+
+const buttons = document.querySelectorAll('.btn')[0];
+
+console.log(buttons);
